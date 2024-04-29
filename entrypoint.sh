cb627b65e58aed374a3bbca64b843df6e7a59d6c
César Benjamín <mathereall@gmail.com>
2024-04-29 13:18:24 -0600

    gem net http

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# https://stackoverflow.com/a/38732187/1935918
 set -e
 
 # Decidim
 if [ -f /decidim/tmp/pids/server.pid ]; then
-  rm /decidim/tmp/pids/server.pid
+  rm -rf /decidim/tmp/pids/server.pid
 fi
 exec bundle exec "$@"
